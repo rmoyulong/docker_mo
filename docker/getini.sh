c5477a25d8bb25a0b009473bb79e78bb2c6bf809
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-26 16:09:57 +0800

    Update getini.sh

--- docker/getini.sh
+++ docker/getini.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-svn export https://github.com/rmoyulong/hubdocker_onecloud/trunk/docker ./hub/docker
+svn export https://github.com/rmoyulong/hubdocker_onecloud/trunk/docker hub/docker
